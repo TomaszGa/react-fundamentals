a7dfebc7d9adbe0a51ab59046633bb0d823ce9d2
TomaszGa <tomasz.galina@gmail.com>
2017-06-05 19:32:46 +0200

    add prop-types to app

--- app/index.js
+++ app/index.js
@@ -1,26 +1,50 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
+var PropTypes = require('prop-types');
 require('./index.css');
 
 
 class App extends React.Component {
 	render() {
+    {var friend = this.props.list.filter(function(obj){
+            return obj.friend === true})}
+    {var notFriend = this.props.list.filter(function(obj){
+            return obj.friend !== true})}    
 		return(
       <div>
-			<div>
-			Hello React and why do you take so damn long!
-			</div>
-      <div>
-      Oh so you do not lol
-      <p>How slow would this be</p>
-      <p>Preeeety quick lol</p>
-      </div>
+        <h1>Friends</h1>
+        <ul>
+          {friend.map(function(person){
+            return <li key={person.name}> {person.name} </li>})}
+        </ul>
+        <hr />
+        <ul>
+          {notFriend.map(function(person){
+            return <li key={person.name}> {person.name} </li>})}
+        </ul>       
       </div>
 		)
 	}
 }
 
+App.propTypes = {
+  list: PropTypes.arrayOf(PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    friend: PropTypes.bool.isRequired,
+
+  })),
+}
+
+
 ReactDOM.render(
-  <App />,
+  <App list={[
+    { name: 'Tyler', friend: true },
+    { name: 'Ryan', friend: true },
+    { name: 'Michael', friend: false },
+    { name: 'Mikenzi', friend: false },
+    { name: 'Jessica', friend: true },
+    { name: 'Dan', friend: false } ]} />,
   document.getElementById('app')
 );
+
+
