d7fa381d5979becfbcb5bdd46d25912b5b5ee029
TomaszGa <tomasz.galina@gmail.com>
2017-06-09 22:17:28 +0200

    add react-router

--- app/components/app.js
+++ app/components/app.js
@@ -1,6 +1,8 @@
 var React = require('react');
 var Popular = require('./Popular');
-
+var ReactRouter = require('react-router-dom');
+var Router = ReactRouter.BrowserRouter;
+var Route = ReactRotuer.Route;
 class App extends React.Component {
   render() {
      return (
